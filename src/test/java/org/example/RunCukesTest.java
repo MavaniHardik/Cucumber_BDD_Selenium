3fb4d4bbd9161d753aa6378a5f46c3dfc88ea236
MavaniHardik <hardikmavani96@gmail.com>
2023-12-23 10:55:44 +0000

    update login

--- src/test/java/org/example/RunCukesTest.java
+++ src/test/java/org/example/RunCukesTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;;
 dryRun = false,
 strict = true,
 monochrome = true,
-tags = "@SingleRegisration",
+tags = "@ValidateLogin",
 plugin = "json:target/cucumber-report/cucumber.json")
 public class RunCukesTest {
     //cucumber Test
